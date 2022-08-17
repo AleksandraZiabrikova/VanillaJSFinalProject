09120a39720c54eae1a4389237edd6b19da21fb6
AleksandraZiabrikova <109486703+AleksandraZiabrikova@users.noreply.github.com>
2022-08-17 17:14:56 +0300

    Weather icon

--- src/app.js
+++ src/app.js
@@ -28,18 +28,24 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
-
+  let iconElement = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   conditionsElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "1ddfe6760d70aea0fb7088fd8803337f";
 let endPoint = "https://api.openweathermap.org/data/2.5/weather?";
 let unit = "metric";
-let apiUrl = `${endPoint}q=New York&units=${unit}&appId=${apiKey}`;
+let city = "Berlin";
+let apiUrl = `${endPoint}q=${city}&units=${unit}&appId=${apiKey}`;
 
 axios.get(apiUrl).then(displayTemperature);
